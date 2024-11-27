9b8c9727982db64c60cf611d0120fd72216ffdb4
Rob <king.roob@gmail.com>
2024-11-27 23:53:37 +0000

    feat: set up nestjs project

---
+++ nest-proj/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { CatsController } from './cats/cats.controller';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [AuthModule, UsersModule],
+  controllers: [AppController, CatsController, AuthController],
+  providers: [AppService, AuthService],
+})
+export class AppModule { }
